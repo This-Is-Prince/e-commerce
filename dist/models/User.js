f49b13054e963b00493e2b22b2bc64ef66367eb3
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-08-30 11:32:35 +0530

    feat: user schema added

---
+++ dist/models/User.js
@@ -0,0 +1,35 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = require("mongoose");
+const validator_1 = __importDefault(require("validator"));
+const UserSchema = new mongoose_1.Schema({
+    name: {
+        type: String,
+        required: [true, "Please provide name"],
+        minlength: 3,
+        maxlength: 50,
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide email"],
+        validate: {
+            validator: validator_1.default.isEmail,
+            message: "Please provide valid email",
+        },
+    },
+    password: {
+        type: String,
+        required: [true, "Please provide password"],
+        minlength: 6,
+    },
+    role: {
+        type: String,
+        enum: ["admin", "user"],
+        default: "user",
+    },
+});
+const User = (0, mongoose_1.model)("User", UserSchema);
+exports.default = User;
---
+++ src/models/User.ts
@@ -0,0 +1,33 @@
+import { model, Schema } from "mongoose";
+import validator from "validator";
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide name"],
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide email"],
+    validate: {
+      validator: validator.isEmail,
+      message: "Please provide valid email",
+    },
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password"],
+    minlength: 6,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
+  },
+});
+
+const User = model("User", UserSchema);
+
+export default User;
